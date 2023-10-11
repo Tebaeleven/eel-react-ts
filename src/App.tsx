260e9837c209aeb1ffb147a4d3d03b85ff7e9b92
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-10-11 13:43:28 +0900

    eel側の関数を呼び出せるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+export const eel = (window as any).eel;
+eel.set_host("ws://localhost:8080");
+
+function test() {
+    eel.test();
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <button onClick={test}>python関数を実行</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
